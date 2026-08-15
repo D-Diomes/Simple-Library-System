d266e6f170d2b1a811d88471519e100cb565f78d
erikmanarang19 <erikmanarang19@gmail.com>
2026-08-15 06:12:57 +0000

    Added file but not finish yet

---
+++ ADD.py
@@ -0,0 +1,13 @@
+books = []
+
+def add_book():
+    title = input ("Enter book title:")
+    author = input ("Enter author name:")
+
+    book = {
+    "title": title,
+     "author": author   
+ }
+
+    books.append(book)
+print ("Book Added Successfully!")
