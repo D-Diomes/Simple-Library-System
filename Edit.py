dec5b22e5baaf0eedbe1bfc1d1aca88dbb2830e6
D-Diomes <diomesd09@gmail.com>
2026-08-15 06:19:32 +0000

    Edit.py

---
+++ Edit.py
@@ -0,0 +1,19 @@
+Books = {
+    "book1" : {
+        "Name" : "Punisher Max: In the Beginning"
+        "Genre" : "Crime"
+        "year" : "2004"
+    }
+    "book2" : {
+        "Name" : "Captain America: The Winter Soldier"
+        "Genre" : "Adventure"
+        "year" : "2005"
+    }
+    "book3" : {
+        "Name" : "Black Widow: The Name of the Rose"
+        "Genre" : "Action"
+        "year" : "2010"
+    }
+}
+
+print(Books["book1"]["name"])
