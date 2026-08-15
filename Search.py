9f5661b91e369f60b74e4369898e31bf03291611
Montes Danielle F <dafemon19@gmail.com>
2026-08-15 06:33:45 +0000

    initial update

---
+++ Search.py
@@ -0,0 +1,42 @@
+def search_Book(book_list, item_to_find):
+    if item_to_find in book_list:
+
+    
+        return f"{item_to_find} is found"
+       
+
+    else:
+        return f"{item_to_find} was not found."
+
+    
+def search_Genre(genre_list, item_to_find):
+    if item_to_find in genre_list:
+
+    
+        return f"{item_to_find} is found"
+       
+
+    else:
+        return f"{item_to_find} was not found."
+
+
+def search_Year(year_list, item_to_find):
+    if item_to_find in year_list:
+
+    
+        return f"{item_to_find} is found"
+       
+
+    else:
+        return f"{item_to_find} was not found."
+
+
+def search();
+  if 
+  print("Enter the name of the book: ")
+  Search = input()
+
+  
+
+books = ["The Odyssey","Harry Potter","1987"]
+print(search_Book,search_Genre,search_Year,(Search))  
