2bcd80b3fa44898578ecf834f9a90d012710eab6
D-Diomes <diomesd09@gmail.com>
2026-08-15 06:32:56 +0000

    Edit.py

--- Edit.py
+++ Edit.py
@@ -1,19 +1,32 @@
 Books = {
     "book1" : {
-        "Name" : "Punisher Max: In the Beginning"
-        "Genre" : "Crime"
-        "year" : "2004"
-    }
+        "Name" : "Punisher Max: In the Beginning",
+        "Genre" : "Crime",
+        "year" : 2004
+    },
     "book2" : {
-        "Name" : "Captain America: The Winter Soldier"
-        "Genre" : "Adventure"
-        "year" : "2005"
-    }
+        "Name" : "Captain America: The Winter Soldier",
+        "Genre" : "Adventure",
+        "year" : 2005
+    },
     "book3" : {
-        "Name" : "Black Widow: The Name of the Rose"
-        "Genre" : "Action"
-        "year" : "2010"
+        "Name" : "Black Widow: The Name of the Rose",
+        "Genre" : "Action",
+        "year" : 2010
     }
 }
 
-print(Books["book1"]["name"])
+def Edit():
+    print("1. Edit book name")
+    print("2. Edit book genre")
+    print("3. Edit book year")
+
+    if num = 1:
+        print
+
+
+
+
+while True:
+    (Edit)
+    break
